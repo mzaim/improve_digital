e1f832458aba4c488c0ced2888a48bc82892eb20
mirela <->
2018-04-28 17:42:42 +0300

    -

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+Assumptions:
+
+1. Words are separated by spaces, punctuation does not exist
+2. Input files are in classpath
---
+++ src/main/java/digital/FilePaths.java
@@ -0,0 +1,6 @@
+package digital;
+
+public class FilePaths {
+    public static String FILE1 = "src\\main\\resources\\file1";
+    public static String FILE2 = "src\\main\\resources\\file2";
+}
